88486fe0c5563d267e86b1fb0e616a8bfd099a20
EthanQuist <ethantquist@gmail.com>
2019-11-17 19:17:57 -0800

    Updated comments to follow doxygen style

--- src/walker.cpp
+++ src/walker.cpp
@@ -1,8 +1,47 @@
+/* Copyright (c) 2019 Ethan Quist
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @file walker.cpp
+ *
+ * @brief This is a ros code to let the turtlebot roam around the world
+ *
+ * @author Ethan Quist
+ *
+ * @copyright Ethan Quist
+ *
+ *
+ */
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <stdlib.h>
 
+/**
+ * @brief This class handles publishing velocity and subscribing to Bumper Events
+ *
+ */
 class myTurtleClass {
  public:
   myTurtleClass() {
@@ -15,8 +54,16 @@ class myTurtleClass {
                         &myTurtleClass::callback, this);
   }
 
+  /**
+   * @brief this is the callback function for the subscriber node
+   *
+   * @param a pointer to the subscribed message
+   *
+   * @return none
+   */
   void callback(const kobuki_msgs::BumperEvent::ConstPtr& input) {
     ROS_INFO_STREAM("Uh oh!!");
+    //When bumpevent happens turn the robot instead of going straight
     geometry_msgs::Twist output;
     //make turtle turn
     //don't move straight
@@ -42,7 +89,9 @@ int main(int argc, char **argv) {
   myTurtleClass myTurtleObject;
 
   //Run when not bumping
-  ros::NodeHandle nh;
+  ros::NodeHandle nh;  //creating the node handler
+
+  //publishing the veloctity to the turtlebot to move straight
   ros::Publisher pub = nh.advertise < geometry_msgs::Twist
       > ("/mobile_base/commands/velocity", 10);
 
@@ -55,6 +104,7 @@ int main(int argc, char **argv) {
     //don't turn
     msg.angular.z = 0;
 
+    //publish the velocity to the turtlebot
     pub.publish(msg);
 
     ROS_INFO_STREAM(
